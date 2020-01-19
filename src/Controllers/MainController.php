7de2a7a319faf6a55845103bbeb45f4fa5e5f120
donjmi <jldonne@gmail.com>
2020-01-19 23:15:45 +0100

    MainController.php with twig

---
+++ src/Controllers/MainController.php
@@ -0,0 +1,46 @@
+<?php
+namespace Blog\Controllers;
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use Blog\Models\ArticlesModel;
+
+abstract class MainController
+{
+    protected $twig = null;
+
+    /**
+     * MainController constructor
+     */
+    public function __construct()
+    {
+        $loader = new FilesystemLoader(ROOT.'src/views');
+        $this->twig = new Environment($loader, [
+          'cache' => false,  //__DIR__ .'/tmp'
+        ]); 
+    }
+
+    /**
+     * Allows to load a model
+     * @param string $model
+     * @return void
+     */
+    public function loadModel(string $model)
+    {      
+        // create an instance of this model
+        $model = 'Blog\\Models\\' . $model . 'Model';
+        return new $model();
+    }
+
+    /**
+     * extract($data) allows you to create a variable for each field
+     * call twig function render
+     */
+    public function render(string $file, array $data = [])
+    {
+        extract($data);
+ 
+        echo $this->twig->render($file . '.twig', $data);
+        
+    }
+}

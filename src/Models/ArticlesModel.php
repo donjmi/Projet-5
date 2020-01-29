9424cfd08ed3dc9fe6f8f72749901e9051021304
donjmi <jldonne@gmail.com>
2020-01-29 02:01:00 +0100

    add read function  Model

--- src/Models/ArticlesModel.php
+++ src/Models/ArticlesModel.php
@@ -11,4 +11,12 @@ public function __construct()
     // Nous ouvrons la connexion à la base de données
     $this->getConnection();
 }
+
+public function getOne($id){
+    $req = "SELECT * FROM ". $this->table ." WHERE id='". $id ."'";
+    // $req = "SELECT * FROM ". $this->table ." WHERE id='10'";
+    $query = $this->_connexion->prepare($req);
+    $query->execute();
+    return $query->fetch();
+}
 }

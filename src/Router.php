2c76bf252c8c9c0b78e451be91f62bd968fec617
donjmi <jldonne@gmail.com>
2020-02-01 01:55:19 +0100

    add homepage in Router

--- src/Router.php
+++ src/Router.php
@@ -10,34 +10,36 @@ class Router
         // define('ROOT', dirname(__DIR__));
 
         // On sépare les paramètres et on les met dans le tableau $params
-        $params = explode('/', filter_var($_GET['page'], FILTER_SANITIZE_URL));
+        $url = explode('/', filter_var($_GET['page'], FILTER_SANITIZE_URL));
 
         // Si au moins 1 paramètre existe
-        if ($params[0] != "") {
+        if ($url[0] != "") {
             // On sauvegarde le 1er paramètre dans $controller en mettant sa 1ère lettre en majuscule
-            $controller = ucfirst($params[0]);
-            // var_dump($controller);
+            $controller = ucfirst($url[0]);
+          
             // On sauvegarde le 2ème paramètre dans $action s'il existe, sinon la méthode index()
-            $action = isset($params[1]) ? $params[1] : 'index';
+            $action = isset($url[1]) ? $url[1] : 'index';
 
             // On appelle le contrôleur
-            // $ctrl = $controller . "Controller";
-            require_once(ROOT.'\src/Controllers/' . $controller . '.php');
+            require_once(ROOT.'src/Controllers/' . $controller . '.php');
 
             // On instancie le contrôleur
-            $controller = new Controllers\ArticlesController();
+
+            $controller = 'Blog\\Controllers\\' . $controller;
+            $controller = new $controller();
+          
 
             // On appelle la méthode
             if (method_exists($controller, $action)) {
                 /**
                  * unset : permet d'initialiser la variable $params
                  */
-                unset($params[0]);
-                unset($params[1]);
+                unset($url[0]);
+                unset($url[1]);
 
                 // appelle une fonction le controlleur et la méthode en ajoutant des paramètres
-                call_user_func_array([$controller, $action], $params);
-                // $controller->$action();
+                call_user_func_array([$controller, $action], $url);
+
             } else {
                 http_response_code(404);
                 echo "La page recherchée n'existe pas";
@@ -45,9 +47,12 @@ class Router
 
         } else {
             
-            // Ici aucun paramètre n'est défini ->  renvoyer sur page default
-            // require;
+            require_once(ROOT.'src/Controllers/HomeController.php');
 
-        }//end if
-    } //end function run
-}//end class Router
+            // On instancie le contrôleur
+            $controller = 'Blog\\Controllers\\HomeController'; 
+            $controller = new $controller();
+            $controller->index();
+        }
+    }
+}

2c4b8dad5838285254ff1a3d162574abdab6579c
donjmi <jldonne@gmail.com>
2020-02-01 01:59:35 +0100

    add HomeController-Model-page

---
+++ src/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+namespace Blog\Controllers;
+
+use Blog\Models\HomeModel;
+
+class HomeController extends MainController
+{
+
+    public function index()
+    {
+        /**
+         * load the model and his function
+         */
+        $articles = $this->loadModel("Articles")->getAll();
+            
+        /**
+         * page display 'index' 
+         * articles use to view in index file
+         */
+        $this->render('homepage', ['articles' => $articles]);  
+    }
+
+    public function read($id){
+        // echo  "l'identifiant est : ".$id;
+        $article = $this->loadModel("Articles")->getOne($id);
+        // debug($article);
+        $this->render('homepage3', ['article' => $article]); 
+    }
+}
---
+++ src/Models/HomeModel.php
@@ -0,0 +1,22 @@
+<?php
+namespace Blog\Models;
+
+class HomeModel extends MainModel{
+
+public function __construct()
+{
+    // Nous définissons la table par défaut de ce modèle
+    $this->table = "posts";
+
+    // Nous ouvrons la connexion à la base de données
+    $this->getConnection();
+}
+
+public function getOne($id){
+    $req = "SELECT * FROM ". $this->table ." WHERE id='". $id ."'";
+    // $req = "SELECT * FROM ". $this->table ." WHERE id='10'";
+    $query = $this->_connexion->prepare($req);
+    $query->execute();
+    return $query->fetch();
+}
+}

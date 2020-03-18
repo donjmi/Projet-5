fcb51649cce224f383d683679c7cc035a1903363
donjmi <jldonne@gmail.com>
2020-03-18 14:25:18 +0100

    Usersmodel add

---
+++ src/Models/UsersModel.php
@@ -0,0 +1,16 @@
+<?php
+namespace Blog\Models;
+
+class ArticlesModel extends MainModel{
+public $id;
+
+public function __construct()
+{
+    // Nous définissons la table par défaut de ce modèle
+    $this->table = "users";
+
+    // Nous ouvrons la connexion à la base de données
+    $this->getConnection();
+}
+
+}

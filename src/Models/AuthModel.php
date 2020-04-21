89c0abe1b4fe21025bbbe5b11bfcce2d9126dc5b
donjmi <jldonne@gmail.com>
2020-04-22 01:52:59 +0200

    Init Authentifications

---
+++ src/Models/AuthModel.php
@@ -0,0 +1,26 @@
+<?php
+namespace Blog\Models;
+
+/**
+ * ArticlesModel
+ */
+class AuthModel extends MainModel{
+    public $id;
+
+    public function __construct()
+    {
+        // Nous définissons la table par défaut de ce modèle
+        $this->table = "users";
+
+        // Nous ouvrons la connexion à la base de données
+        $this->getConnection();
+    }
+
+    public function getPass($email)
+    {
+        $req = "SELECT password FROM " . $this->table . " WHERE email='" . $email . "'";
+        $query = $this->connexion->prepare($req);
+        $query->execute();
+        return $query->fetch();
+    }
+}

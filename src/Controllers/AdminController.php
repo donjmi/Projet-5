190d49aa38ecde80af83967082e0da94e38f4c68
donjmi <jldonne@gmail.com>
2020-05-01 13:27:32 +0200

    Backend : admin page init

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -2,46 +2,15 @@
 namespace Blog\Controllers;
 
 use Blog\Models\MainModel;
+use Blog\Models\ArticlesModel;
 
 class AdminController extends MainController
 {    
-    /**
-     * edit
-     *
-     * @return void
-     */
     public function edit()
-    {
-        $post = filter_input_array(INPUT_POST);
-        if (array_key_exists('id', $post) && ! empty($post['id'])){
-            
-            $data= array();
-            $data['id']          = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_SPECIAL_CHARS);
-            $data['title']          = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_SPECIAL_CHARS);
-            $data['slug']           = filter_input(INPUT_POST, 'slug', FILTER_SANITIZE_SPECIAL_CHARS);
-            $data['content']        = filter_input(INPUT_POST, 'content', FILTER_SANITIZE_SPECIAL_CHARS);;
-            $data['date_creation']  = htmlspecialchars(date("Y-m-d H:i:s"));
-            $data['url_images']     = filter_input(INPUT_POST, 'url_images', FILTER_SANITIZE_STRING);
-            // debug($data);
-            $article = MainModel::loadModel("Admin")->update($data);
-
-        } elseif (array_key_exists('title', $post)){
-            
-            $data= array();
-            $data['title']          = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_SPECIAL_CHARS);
-            $data['slug']           = filter_input(INPUT_POST, 'slug', FILTER_SANITIZE_SPECIAL_CHARS);
-            $data['content']        = filter_input(INPUT_POST, 'content', FILTER_SANITIZE_SPECIAL_CHARS);;
-            $data['date_creation']  = htmlspecialchars(date("Y-m-d H:i:s"));
-            $data['url_images']     = filter_input(INPUT_POST, 'url_images', FILTER_SANITIZE_STRING);
-
-            $article = MainModel::loadModel("Admin")->create($data);
-
-        }
-
-        $articles = MainModel::loadModel("Admin")->getAll();
-        $this->render('admin/Admin_post', Array(
-            'articles'  => $articles
-        )); 
-    }
+    {  
+            $titleAdm = "Accueil de l'administration du site";
+            $this->render('admin/Admin_index', Array(
+                'titleAdm'  => $titleAdm));
+    }   
     
 }
--- src/Controllers/ArticlesController.php
+++ src/Controllers/ArticlesController.php
@@ -1,7 +1,6 @@
 <?php
 namespace Blog\Controllers;
 
-use Blog\Models\ArticlesModel;
 use Blog\Controllers\CommentsController;
 use Blog\Models\MainModel;
 
@@ -11,6 +10,47 @@ use Blog\Models\MainModel;
  */
 class ArticlesController extends MainController
 {    
+    /**
+     * edit
+     *
+     * @return void
+     */
+    public function edit()
+    {
+        $post = filter_input_array(INPUT_POST);
+        if (array_key_exists('id', $post) && ! empty($post['id'])){
+            
+            $data= array();
+            $data['id']          = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_SPECIAL_CHARS);
+            $data['title']          = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_SPECIAL_CHARS);
+            $data['slug']           = filter_input(INPUT_POST, 'slug', FILTER_SANITIZE_SPECIAL_CHARS);
+            $data['content']        = filter_input(INPUT_POST, 'content', FILTER_SANITIZE_SPECIAL_CHARS);;
+            $data['date_creation']  = htmlspecialchars(date("Y-m-d H:i:s"));
+            $data['url_images']     = filter_input(INPUT_POST, 'url_images', FILTER_SANITIZE_STRING);
+            // debug($data);
+            MainModel::loadModel("Admin")->update($data);
+
+        } elseif (array_key_exists('title', $post)){
+            
+            $data= array();
+            $data['title']          = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_SPECIAL_CHARS);
+            $data['slug']           = filter_input(INPUT_POST, 'slug', FILTER_SANITIZE_SPECIAL_CHARS);
+            $data['content']        = filter_input(INPUT_POST, 'content', FILTER_SANITIZE_SPECIAL_CHARS);;
+            $data['date_creation']  = htmlspecialchars(date("Y-m-d H:i:s"));
+            $data['url_images']     = filter_input(INPUT_POST, 'url_images', FILTER_SANITIZE_STRING);
+
+            MainModel::loadModel("Admin")->create($data);
+
+        }
+
+        $articles = MainModel::loadModel("Admin")->getAll();
+        $this->render('admin/Admin_post', Array(
+            'articles'  => $articles
+        )); 
+    }
+    
+    
+    
     /**
      * read the article or create the comment article
      *
